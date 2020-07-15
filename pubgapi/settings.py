795e305a4ccabb9a091440b5f9ddc41b996c9320
ranjitkshah <ranjitmaheshshah@gmail.com>
2020-07-15 05:55:03 +0530

    'add-requirement'

--- pubgapi/settings.py
+++ pubgapi/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/images/'
+# MEDIA_URL = '/images/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# # MEDIA_URL='/media/'
