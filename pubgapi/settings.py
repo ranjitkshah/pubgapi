9268365a5ca98fa2538f3f9f153d7cb9d89d3912
ranjitkshah <ranjitmaheshshah@gmail.com>
2020-07-15 06:55:14 +0530

    ninth attempt

--- pubgapi/settings.py
+++ pubgapi/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
